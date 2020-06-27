0b2a399683a5a78965a8032e7af87ab14a7be1f9
Francis Joseph Serina <francis.serina@gmail.com>
2020-06-27 16:39:57 +0800

    Added Heap and HeapTester

---
+++ Assets/Heap/Heap.cs
@@ -0,0 +1,221 @@
+﻿// Modified from https://gist.github.com/roufamatic/ee7e11469809f2b276c0d3dc6b8dd80b
+
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Array-based Heap implementation.
+/// </summary>
+/// <typeparam name="T">Kind of thing being stored in the heap.</typeparam>
+public class Heap<T> where T : IComparable
+{
+    private T[] _heap = null;
+    private Dictionary<T, int> _indexMap;
+    private Comparison<T> _comparer;
+
+    private int DefaultComparer(T x, T y)
+    {
+        return x.CompareTo(y);
+    }
+
+    /// <summary>
+    /// Create a new heap.
+    /// </summary>
+    /// <param name="minSize">The minimum number of elements the heap is expected to hold.</param>
+    public Heap(int minSize)
+    {
+        _heap = new T[((int)Math.Pow(2, Math.Ceiling(Math.Log(minSize, 2))))];
+        _indexMap = new Dictionary<T, int>();
+        _comparer = new Comparison<T>(DefaultComparer);
+    }
+
+    /// <summary>
+    /// Create a new heap.
+    /// </summary>
+    /// <param name="comparer">Custom Comparison delegate</param>
+    /// <param name="minSize">The minimum number of elements the heap is expected to hold.</param>
+    public Heap(Comparison<T> comparer, int minSize)
+    {
+        _heap = new T[((int)Math.Pow(2, Math.Ceiling(Math.Log(minSize, 2))))];
+        _indexMap = new Dictionary<T, int>();
+        _comparer = comparer;
+    }
+
+    /// <summary>
+    /// Current size of the Heap.
+    /// </summary>
+    public int Count { get; private set; } = 0;
+
+    /// <summary>
+    /// Test to see if the Heap is empty.
+    /// </summary>
+    public bool IsEmpty { get { return Count == 0; } }
+
+    /// <summary>
+    /// Add a new value to the Heap.
+    /// </summary>
+    /// <param name="val">The key to be inserted. Sh</param>
+    public void Push(T val)
+    {
+        if (Exists(val))
+        {
+            throw new ArgumentException("Heap does not allow duplicate values");
+        }
+
+        if (Count == _heap.Length)
+        {
+            DoubleHeap();
+        }
+
+        _heap[Count] = val;
+        _indexMap[val] = Count;
+        ShiftUp(Count);
+        Count++;
+    }
+
+    /// <summary>
+    /// View the value currently at the top of the Heap.
+    /// </summary>
+    /// <returns></returns>
+    public T Peek()
+    {
+        if (_heap.Length == 0)
+        {
+            throw new ArgumentOutOfRangeException("No values in heap");
+        }
+        return _heap[0];
+    }
+
+    /// <summary>
+    /// Remove the value currently at the top of the Heap and return it.
+    /// </summary>
+    /// <returns></returns>
+    public T Pop()
+    {
+        T output = Peek();
+        _indexMap.Remove(output);
+
+        Count--;
+
+        _heap[0] = _heap[Count];
+        _indexMap[_heap[0]] = 0;
+
+        ShiftDown(0);
+        return output;
+    }
+
+    /// <summary>
+    /// Checks if the key exists.
+    /// </summary>
+    /// <param name="val">The key to test</param>
+    /// <returns>True if the key already exists. False otherwise</returns>
+    public bool Exists(T val)
+    {
+        return _indexMap.ContainsKey(val);
+    }
+
+    /// <summary>
+    /// Increases the value of the key.
+    /// </summary>
+    /// <param name="val">The new value of the key</param>
+    public void IncreaseKey(T val)
+    {
+        var index = _indexMap[val];
+
+        if (_comparer(_heap[index], val) <= 0)
+        {
+            throw new ArgumentException("Key is expected to increase");
+        }
+
+        _heap[index] = val;
+        ShiftDown(index);
+    }
+
+    /// <summary>
+    /// Decreases the value of the key.
+    /// </summary>
+    /// <param name="val">The new value of the key</param>
+    public void DecreaseKey(T val)
+    {
+        var index = _indexMap[val];
+
+        if (_comparer(_heap[index], val) >= 0)
+        {
+            throw new ArgumentException("Key is expected to decrease");
+        }
+
+        _heap[index] = val;
+        ShiftUp(index);
+    }
+
+    /// <summary>
+    /// Move an element up the Heap.
+    /// </summary>
+    /// <param name="heapIndex"></param>
+    private void ShiftUp(int heapIndex)
+    {
+        if (heapIndex == 0)
+        {
+            return;
+        }
+        var parentIndex = (heapIndex - 1) / 2;
+        if (_comparer(_heap[parentIndex], _heap[heapIndex]) > 0)
+        {
+            Swap(parentIndex, heapIndex);
+            ShiftUp(parentIndex);
+        }
+    }
+
+    /// <summary>
+    /// Move an element down the Heap.
+    /// </summary>
+    /// <param name="heapIndex"></param>
+    private void ShiftDown(int heapIndex)
+    {
+        var child1 = heapIndex * 2 + 1;
+        if (child1 >= Count)
+        {
+            return;
+        }
+        var child2 = child1 + 1;
+
+        var preferredChildIndex = child1;
+        if (child2 < Count && _comparer(_heap[child1], _heap[child2]) > 0)
+        {
+            preferredChildIndex = child2;
+        }
+
+        if (_comparer(_heap[preferredChildIndex], _heap[heapIndex]) > 0)
+        {
+            return;
+        }
+
+        Swap(heapIndex, preferredChildIndex);
+        ShiftDown(preferredChildIndex);
+    }
+
+    /// <summary>
+    /// Swap two items in the underlying array.
+    /// </summary>
+    /// <param name="index1"></param>
+    /// <param name="index2"></param>
+    private void Swap(int index1, int index2)
+    {
+        T temp = _heap[index1];
+        _heap[index1] = _heap[index2];
+        _heap[index2] = temp;
+
+        _indexMap[_heap[index2]] = index2;
+        _indexMap[_heap[index1]] = index1;
+    }
+
+    /// <summary>
+    /// Increase the size of the underlying storage.
+    /// </summary>
+    private void DoubleHeap()
+    {
+        var copy = new T[_heap.Length * 2];
+        Array.Copy(_heap, copy, _heap.Length);
+        _heap = copy;
+    }
+}
---
+++ Assets/Heap/HeapTester.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HeapTester : MonoBehaviour
+{
+    private struct Element<K, V> : System.IComparable where V : System.IComparable
+    {
+        private readonly K _key;
+        private V _val;
+
+        public Element(K key, V value)
+        {
+            _key = key;
+            _val = value;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var other = (Element<K, V>)obj;
+            return _val.CompareTo(other._val);
+        }
+
+        public override int GetHashCode()
+        {
+            return _key.GetHashCode();
+        }
+    }
+
+    private delegate bool TestCase();
+
+    private void Start()
+    {
+        var tests = new Dictionary<string, TestCase>();
+        tests["Sorting"] = TestSorting;
+        tests["Resizing"] = TestResizing;
+        tests["Custom Sorting"] = TestCustomSorting;
+
+        foreach (var test in tests)
+        {
+            Debug.LogFormat("{0} Test: {1}",
+                test.Key,
+                test.Value() ? "<color=green>Success!</color>" : "<color=red>Failed!</color>");
+        }
+    }
+
+    private bool TestSorting()
+    {
+        var testHeap = new Heap<int>(5);
+
+        //var log = "Input: ";
+
+        for (var i = 0; i < 5; ++i)
+        {
+            var val = Random.Range(0, 100);
+
+            while (testHeap.Exists(val))
+            {
+                val = Random.Range(0, 100);
+            }
+
+            //log += val + ", ";
+            testHeap.Push(val);
+        }
+
+        //log += System.Environment.NewLine;
+        //log += "Output: ";
+
+        var lastVal = -101;
+        while (!testHeap.IsEmpty)
+        {
+            var val = testHeap.Pop();
+            if (lastVal >= val)
+            {
+                return false;
+            }
+            lastVal = val;
+            //log += val + ", ";
+        }
+        //log += System.Environment.NewLine;
+
+        //Debug.Log(log);
+        return true;
+    }
+
+    private bool TestResizing()
+    {
+        var testHeap = new Heap<int>(2);
+
+        for (var i = 0; i < 10; ++i)
+        {
+            var val = Random.Range(0, 100);
+
+            while (testHeap.Exists(val))
+            {
+                val = Random.Range(0, 100);
+            }
+
+            testHeap.Push(val);
+        }
+
+        var lastVal = -101;
+        while (!testHeap.IsEmpty)
+        {
+            var val = testHeap.Pop();
+            if (lastVal >= val)
+            {
+                return false;
+            }
+            lastVal = val;
+        }
+
+        return true;
+    }
+
+    private int ReverseComparer(int x, int y)
+    {
+        return -x.CompareTo(y);
+    }
+
+    private bool TestCustomSorting()
+    {
+        var testHeap = new Heap<int>(new System.Comparison<int>(ReverseComparer), 2);
+
+        for (var i = 0; i < 10; ++i)
+        {
+            var val = Random.Range(0, 100);
+
+            while (testHeap.Exists(val))
+            {
+                val = Random.Range(0, 100);
+            }
+
+            testHeap.Push(val);
+        }
+
+        var lastVal = 101;
+        while (!testHeap.IsEmpty)
+        {
+            var val = testHeap.Pop();
+            if (lastVal <= val)
+            {
+                return false;
+            }
+            lastVal = val;
+        }
+
+        return true;
+    }
+
+}
